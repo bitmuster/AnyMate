512386550057415d70c3563b5c239340134ec091
Michael Abel <micha@proteus.localdomain>
2017-02-12 00:05:18 +0100

    Added test

--- test_AnyMate.py
+++ test_AnyMate.py
@@ -1,10 +1,21 @@
 
 import unittest
+from AnyMate import *
 
-class TestAnyMate(unittest.TestCase):
+class TestAnyMateConfig(unittest.TestCase):
 
     def test_pass(self):
         self.assertEqual( 1, 1)
+
+    def test_init(self):
+        c=Config("text","name","command","color","envobj")
+        self.assertEqual(c.text,"text")
+        self.assertEqual(c.name,"name")
+        self.assertEqual(c.command,"command")
+        self.assertEqual(c.color,"color")
+        self.assertEqual(c.envobj,"envobj")
+
+
 
 if __name__ == '__main__':
     unittest.main()
