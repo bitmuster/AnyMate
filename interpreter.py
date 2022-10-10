e829cd1ddf6bf062f7e0129f67520012507eefdf
Michael Abel <mabel@bitmuster.org>
2022-10-10 18:57:17 +0200

    Disable all unsupported interpreters

--- interpreter.py
+++ interpreter.py
@@ -12,44 +12,45 @@ class Interpreter:
             else:
                 self.shell_suffix = """ echo "Sleeping 5 seconds"\n sleep 5' &"""
 
-        elif shell == "xtermpopen":
-            self.shell_prefix = """ ' """
-            if self.wait:
-                self.shell_suffix = (
-                    """echo "Press the Any-Key to Continue "\nread any-key' """
-                )
-            else:
-                self.shell_suffix = """ echo "Sleeping 5 seconds"\n sleep 5' """
 
+#        elif shell == "xtermpopen":
+#            self.shell_prefix = """ ' """
+#            if self.wait:
+#                self.shell_suffix = (
+#                    """echo "Press the Any-Key to Continue "\nread any-key' """
+#                )
+#            else:
+#                self.shell_suffix = """ echo "Sleeping 5 seconds"\n sleep 5' """
+#
         elif shell == "urxvt":
             self.shell_prefix = """urxvt -sl 10000 -cr BLUE -bg lightblue -fg black -e /bin/bash -c ' \n"""
             self.shell_suffix = (
                 """echo "Press the Any-Key to Continue "\nread any-key' &"""
             )
-
-        elif shell == "gnome-terminal":
-            self.shell_prefix = """gnome-terminal --hide-menubar -x /bin/bash -c '\n"""
-            self.shell_suffix = (
-                """echo "Press the Any-Key to Continue "\nread any-key' &"""
-            )
-
-        elif shell == "none":
-            self.shell_prefix = """/bin/bash -c ' \n"""
-            self.shell_suffix = """ ' &"""
-
-        elif shell == "nonepopen":
-            self.shell_prefix = """ """
-            self.shell_suffix = """ """
-
-        elif shell == "none_win":  # Windows cmd.exe without own window
-            self.shell_prefix = """cmd.exe /C  """
-            # self.shell_suffix = \
-            # """ & echo Press the any-key & pause"""
-            self.shell_suffix = """ """
-
-        elif shell == "win":  # Windows cmd.exe in own window
-            self.shell_prefix = """start cmd.exe /C " """
-            self.shell_suffix = """ & echo Press the any-key & pause " """
+#
+#        elif shell == "gnome-terminal":
+#            self.shell_prefix = """gnome-terminal --hide-menubar -x /bin/bash -c '\n"""
+#            self.shell_suffix = (
+#                """echo "Press the Any-Key to Continue "\nread any-key' &"""
+#            )
+#
+#        elif shell == "none":
+#            self.shell_prefix = """/bin/bash -c ' \n"""
+#            self.shell_suffix = """ ' &"""
+#
+#        elif shell == "nonepopen":
+#            self.shell_prefix = """ """
+#            self.shell_suffix = """ """
+#
+#        elif shell == "none_win":  # Windows cmd.exe without own window
+#            self.shell_prefix = """cmd.exe /C  """
+#            # self.shell_suffix = \
+#            # """ & echo Press the any-key & pause"""
+#            self.shell_suffix = """ """
+#
+#        elif shell == "win":  # Windows cmd.exe in own window
+#            self.shell_prefix = """start cmd.exe /C " """
+#            self.shell_suffix = """ & echo Press the any-key & pause " """
 
         else:
             msg = "Shell %s not found." % shell
